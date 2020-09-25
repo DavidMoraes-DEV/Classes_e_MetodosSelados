686cc2eb6ca4d040a707a86ca7aeeecdcbc2f024
David Moraes <investtraderrs@gmail.com>
2020-09-25 13:57:00 -0300

    Classes e Metodos Selados

---
+++ Classes_e_MetodosSelados/Entities/SavingsAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sobreposicao_PalavraVirtual_Override_Base.Entities
+{
+    sealed class SavingsAccount : Account //Essa classe agora com o sealed é selada para que não possa ter uma sub classe derivada dessa.
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public sealed override void Withdraw(double amount) //Agora esse método sobreposto com a palavra sealed NÃO poderá ser sobreposta novamente em outra subclasse.
+        {
+            base.Withdraw(amount); 
+            Balance -= 2.0; 
+        }
+
+    }
+}
---
+++ Classes_e_MetodosSelados/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Classes_e_MetodosSelados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * CLASSES E MÉTODOS SELADOS:
+            
+            - Palavra chave: sealed (Quando for usada é para definir que a classe será selada).
+            - Classe Selada: Evita que a classe seja herdada
+            Obs. Ainda é possível extender a funcionalidade de uma classe selada usando "extension methods".
+
+            Exemplo: namespace Course {
+                        sealed class SavingsAccount {...
+
+            * MÉTODO:
+            
+            - Evita que um método sobreposto possa ser sobreposto novamente.
+            Obs. Só pode ser aplicado a métodos sobrepostos.
+
+            * PRA QUÊ SELAR UMA CLASSE OU MÉTODO?
+            
+            - SEGUNRANÇA: Dependendo das regras do negócio, às vezes é desejável garantir que uma classe não seja herdada, ou que um método não seja sobreposto.
+            Obs. Geralmente convém selar métodos sobrepostos, pois sobreposições múltiplas podem ser uma porta de entrada para inconsistências.
+            
+            - PERFORMANCE: Atributos de tipo de uma classe selada são analisados de forma mais rápida em tempo de execução.
+            Exemplo: Clássico: string.
+            */
+        }
+    }
+}
